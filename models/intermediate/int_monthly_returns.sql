30b4e7df7981bc24a9588fcc6649da9adcf70eff
Louis Jager <safehealing1@gmail.com>
2024-09-10 22:12:35 +0000

    save

--- models/intermediate/int_monthly_returns.sql
+++ models/intermediate/int_monthly_returns.sql
@@ -1,15 +1,15 @@
 WITH stock_data AS (
     SELECT
         TICKER,
-        DATE,
-        CLOSE,
-        FIRST_VALUE(CLOSE) OVER (PARTITION BY TICKER ORDER BY DATE ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_close,
-        LAST_VALUE(CLOSE) OVER (PARTITION BY TICKER ORDER BY DATE ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_close
+        DATE_TRUNC('month', DATE) AS month,
+        FIRST_VALUE(CLOSE) OVER (PARTITION BY TICKER, DATE_TRUNC('month', DATE) ORDER BY DATE) AS first_close,
+        LAST_VALUE(CLOSE) OVER (PARTITION BY TICKER, DATE_TRUNC('month', DATE) ORDER BY DATE) AS last_close
     FROM {{ ref('stg_stock_prices') }}
 )
 
 SELECT
     TICKER,
-    (last_close - first_close) / first_close AS long_term_growth
+    month,
+    (last_close - first_close) / first_close AS monthly_return
 FROM stock_data
-GROUP BY TICKER, first_close, last_close
+WHERE first_close IS NOT NULL AND last_close IS NOT NULL
